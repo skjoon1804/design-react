b8e62a980347f5c3983bd53b73d694f136e47351
skjoon1804 <skjoon1804@naver.com>
2020-12-22 20:54:23 -0800

    Split up speakers into array

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "24874dee3fe4da457367";
+/******/ 	var hotCurrentHash = "45965cf33beea8c8c8db";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,6 +1,24 @@
 import React from 'react'
 
-const Speakers = () => 
-    <img src="images/speakers.png" />;
+const Speakers = () => {
+    const speakers = [
+        {
+            imageSrc : 'speaker-component-1124'
+        },
+        {
+            imageSrc : 'speaker-component-1530'
+        },
+        {
+            imageSrc : 'speaker-component-10803'
+        }
+    ];
 
+    return (
+        <div>
+            {speakers.map((speaker) => {
+                return <img src={`images/${speaker.imageSrc}.png`} key={speaker.imageSrc} />;
+            })}
+        </div>
+    );
+}
 export default Speakers;
