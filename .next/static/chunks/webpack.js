5a93102122b4e3930588b2394bcc3d4b590f2890
skjoon1804 <skjoon1804@naver.com>
2021-01-03 21:24:08 -0800

    Move db over and implement async functions

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a6c21474fa6b62676fe";
+/******/ 	var hotCurrentHash = "b4f80ea4da0f02288b9f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,48 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 import withData from './withData'
 import SpeakerSearchBar from '../SpeakerSearchBar/SpeakerSearchBar'
 import Speaker from '../Speaker/Speaker'
 
 const Speakers = () => {
-    const speakersArr = [
-        {
-            imageSrc: 'speaker-component-1124',
-            name: 'Douglas Crockford',
-            id: 1124,
-            firstName: 'Douglas',
-            lastName: 'Crockford',
-            sat: true,
-            sun: false,
-            isFavorite: false,
-            bio:
-            'Douglas Crockford discovered the JSON Data Interchange Format. He is also the author of _JavaScript: The Good Parts_. He has been called a guru, but he is actually more of a mahatma.',
-        },
-        {
-            imageSrc: 'speaker-component-1530',
-            name: 'Tamara Baker',
-            id: 1530,
-            firstName: 'Tamara',
-            lastName: 'Baker',
-            sat: false,
-            sun: true,
-            isFavorite: true,
-            bio:
-            'Tammy has held a number of executive and management roles over the past 15 years, including VP engineering Roles at Molekule Inc., Cantaloupe Systems, E-Color, and Untangle Inc.',
-        },
-        {
-            imageSrc: 'speaker-component-10803',
-            name: 'Eugene Chuvyrov',
-            id: 10803,
-            firstName: 'Eugene',
-            lastName: 'Chuvyrov',
-            sat: true,
-            sun: false,
-            isFavorite: false,
-            bio:
-            'Eugene Chuvyrov is  a Senior Cloud Architect at Microsoft. He works directly with both startups and enterprises to enable their solutions in Microsoft cloud, and to make Azure better as a result of this work with partners.',
-        },
-    ];
-
     function toggleSpeakerFavorite(speakerRec) {
         return {
             ...speakerRec,
@@ -50,14 +13,23 @@ const Speakers = () => {
         };
     }
 
-    function onFavoriteToggleHandler(speakerRec) {
+    async function onFavoriteToggleHandler(speakerRec) {
         const toggledSpeakerRec = toggleSpeakerFavorite(speakerRec);
         const speakerIndex = speakers.map((speaker) => speaker.id).indexOf(speakerRec.id);
+
+        await axios.put(`http://localhost:3000/speakers/${speakerRec.id}`, toggledSpeakerRec);
         setSpeakers([...speakers.slice(0, speakerIndex), toggledSpeakerRec, ...speakers.slice(speakerIndex+1)]);
     }
 
     const [searchQuery, setSearchQuery] = useState("");
-    const [speakers, setSpeakers] = useState(speakersArr);
+    const [speakers, setSpeakers] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get("http://localhost:3000/speakers");
+            setSpeakers(response.data);
+        }
+        fetchData();
+    }, [])
 
     return (
         <div>
